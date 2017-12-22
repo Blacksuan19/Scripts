b73eafd43a83f2c2dcbd21d644a410d5e9b5f756
Madkita <abubakaryagob@gmail.com>
2017-12-23 03:16:05 +0800

    removed resloution, added cpu temp and free memeory, and more cleanup

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
- 
+# dark shell grave. 
 OS=$(hostnamectl|awk '{$1=$3="";sub(/^[ \t]+/, "")}NR==7')
 HOST=$(hostnamectl|awk '{$1=$2="";sub(/^[ \t]+/, "")}NR==1')
 KERNEL=$(hostnamectl | awk -F- '/Kernel/{ OFS="-";NF--; print }'|awk '{print $3}')
 ARCH=$(uname -m)
 UPTIME=$(awk '{printf("%dd %02dh %02dm",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime)
 MODEL=$(cat /sys/devices/virtual/dmi/id/board_{name,vendor} | awk '!(NR%2){print$1,p}{p=$0}')
-DE=$(plasmashell --version | awk '{print $2}')
-CPU=$(awk < /proc/cpuinfo '/model name/{gsub(/[(GrpcswithRadeon)]/,"");print $4,$5,$6,$8}' | head -1)
+DE=$(plasmashell --version | awk '{print $2}') # i use plasma feel free to change this according to your DE/WM.
+CPU=$(awk < /proc/cpuinfo '/model name/{print $5}' | head -1)
+TEMP=$(neofetch | grep CPU | awk '{print $7}')
 GPU=$(lspci | awk '/VGA/{print $11,$12,$13}' | tr -d '[]')
-# MEMORY=$( free -h | awk '{ $1=$2=$3=$4=$5"";gsub(/[(avilable)]/, ""); print $6}')
-SHELL=$(echo "$SHELL" | awk -F/ '{for ( i=1; i <= NF; i++) sub(".", substr(toupper($i),1,1) , $i); print $NF}')
-RESOLUTION=$(xdpyinfo | awk '/dimensions:/ {printf $2}')
+MEMORY=$(cat /proc/meminfo | grep MemAvailable | awk '$2 { print substr($2/1000/1000,1,4)}')
+SHELL=$(zsh --version | awk '{sub(".", substr(toupper($i),1,1) , $i); print $1" "$2}') # i use zsh if you user another shell change this accordingly.
 BIRTH=$(ls -alct /|sed '$!d'|awk '{print $7, $6, $8}')
-Packages=$(pacman -Q | awk 'END {print NR}')
+Packages=$(pacman -Q | awk 'END {print NR}') #if you dont use arch then what??
 Layout=$(setxkbmap -print | awk -F"+" '/xkb_symbols/{for ( i=1; i <= NF; i++) sub(".", substr(toupper($i),1,1) , $i); print $2}')
-
+clear #clear the screen first before processing output.
  echo  ""
  echo -e "\\e[91m   --------------------"
  echo "   SYSTEM INFORMATION"
  echo "   --------------------"
  echo  ""
- echo -e "\\e[94m     \\e[39m$HOST"
+ # echo -e "\\e[94m     \\e[39m$HOST"
  echo -e "\\e[94m     \\e[39m$MODEL"
  echo -e "\\e[94m     \\e[39m$OS$ARCH"
  echo -e "\\e[94m     \\e[39m$KERNEL"
  echo -e "\\e[94m     \\e[39m$UPTIME"
  echo -e "\\e[94m     \\e[39m$SHELL"
- echo -e "\\e[94m     \\e[39m$RESOLUTION"
- echo -e "\\e[94m     \\e[39m$CPU"
+ echo -e "\\e[94m     \\e[39m$CPU $TEMP"
  echo -e "\\e[94m     \\e[39m$GPU" 
- # echo -e "\\e[94m     \\e[39m$MEMORY"
+ echo -e "\\e[94m     \\e[39m"$MEMORY"G Free" 
  echo -e "\\e[94m   --------------------"
  echo -e "\\e[94m     \\e[39m$DE"
  echo -e "\\e[94m     \\e[39m$BIRTH"
@@ -38,4 +37,3 @@ Layout=$(setxkbmap -print | awk -F"+" '/xkb_symbols/{for ( i=1; i <= NF; i++) su
  echo -e "\\e[94m     \\e[39m$Packages"
  echo -e "\\e[94m     \\e[39m$Layout"
  echo  ""
-
