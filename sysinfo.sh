70782b2db5b04dbd30fc9fd2d39ca3f415fbfce3
Madkita <abubakaryagob@gmail.com>
2017-12-25 02:04:34 +0800

    use proper way to detect cpu temp, added currently playing spotify song

--- sysinfo.sh
+++ sysinfo.sh
@@ -8,13 +8,18 @@ UPTIME=$(awk '{printf("%dd %02dh %02dm",($1/60/60/24),($1/60/60%24),($1/60%60))}
 MODEL=$(cat /sys/devices/virtual/dmi/id/board_{name,vendor} | awk '!(NR%2){print$1,p}{p=$0}')
 DE=$(plasmashell --version | awk '{print $2}') # i use plasma feel free to change this according to your DE/WM.
 CPU=$(awk < /proc/cpuinfo '/model name/{print $5}' | head -1)
-TEMP=$(neofetch | grep CPU | awk '{print $7}')
+TEMP=$(cat /sys/class/hwmon/hwmon0/temp1_input | awk '{print $1 / 1000}')
 GPU=$(lspci | awk '/VGA/{print $11,$12,$13}' | tr -d '[]')
 MEMORY=$(cat /proc/meminfo | grep MemAvailable | awk '$2 { print substr($2/1000/1000,1,4)}')
 SHELL=$(zsh --version | awk '{sub(".", substr(toupper($i),1,1) , $i); print $1" "$2}') # i use zsh if you user another shell change this accordingly.
 BIRTH=$(ls -alct /|sed '$!d'|awk '{print $7, $6, $8}')
 Packages=$(pacman -Q | awk 'END {print NR}') #if you dont use arch then what??
 Layout=$(setxkbmap -print | awk -F"+" '/xkb_symbols/{for ( i=1; i <= NF; i++) sub(".", substr(toupper($i),1,1) , $i); print $2}')
+Playing=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
+            org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
+            string:'Metadata' |\
+            awk -F 'string "' '/string|array/ {printf "%s",$2; next}{print ""}' |\
+            awk -F '"' '/artist/ {a=$2} /title/ {t=$2} END{print a " - " t}')
 clear #clear the screen first before processing output.
  echo  ""
  echo -e "\\e[91m   --------------------"
@@ -27,7 +32,7 @@ clear #clear the screen first before processing output.
  echo -e "\\e[94m     \\e[39m$KERNEL"
  echo -e "\\e[94m     \\e[39m$UPTIME"
  echo -e "\\e[94m     \\e[39m$SHELL"
- echo -e "\\e[94m     \\e[39m$CPU $TEMP"
+ echo -e "\\e[94m     \\e[39m$CPU [$TEMP.0°C]"
  echo -e "\\e[94m     \\e[39m$GPU" 
  echo -e "\\e[94m     \\e[39m"$MEMORY"G Free" 
  echo -e "\\e[94m   --------------------"
@@ -35,5 +40,5 @@ clear #clear the screen first before processing output.
  echo -e "\\e[94m     \\e[39m$BIRTH"
  echo -e "\\e[94m   --------------------"
  echo -e "\\e[94m     \\e[39m$Packages"
- echo -e "\\e[94m     \\e[39m$Layout"
+ echo -e "\\e[94m     \\e[39m$Playing"
  echo  ""
