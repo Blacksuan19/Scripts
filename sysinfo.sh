5ca7284828663a26af7339843c063abb3cea23c4
Madkita <abubakaryagob@gmail.com>
2018-03-31 18:40:47 +0800

    replace date of birth with font, rearrange some options, and updated icons

--- sysinfo.sh
+++ sysinfo.sh
@@ -13,10 +13,10 @@ TEMP=$(cat /sys/class/hwmon/hwmon0/temp1_input | awk '{print $1 / 1000}')
 GPU=$(lspci | awk '/VGA/{print $11,$12,$13}' | tr -d '[]')
 MEMORY=$(cat /proc/meminfo | grep MemAvailable | awk '$2 { print substr($2/1000/1000,1,4)}')
 SHELL=$(zsh --version | awk '{sub(".", substr(toupper($i),1,1) , $i); print $1" "$2}') # i use zsh if you use another shell change this accordingly.
-BIRTH=$(ls -alct --full-time /etc | tail | sed '$!d' | awk '{print $6}')
 Packages=$(pacman -Q | awk 'END {print NR}') # if you dont use arch then what??
 ICONS=$(cat ~/.kde4/share/config/kdeglobals | grep Theme | sed 's/Theme=//g')
 COLORS=$(cat ~/.kde4/share/config/kdeglobals | grep ColorScheme | sed 's/ColorScheme=//g')
+FONT=$(cat ~/.kde4/share/config/kdeglobals | grep font | sed 's/font=//g' | sed 's/,8,-1,5,50,0,0,0,0,0,Medium//g' | sed 's/[][MACR]//g' | sed 's/Neue//g')
 # get currently playing song (spotify and clementine only).
 if pgrep -x "spotify" > /dev/null
 then
@@ -42,9 +42,9 @@ fi
 
 clear # clear the screen first before processing output.
  echo  ""
- echo -e "\\e[91m   --------------------"
- echo "   SYSTEM INFORMATION"
- echo "   --------------------"
+ echo -e "\\e[91m   -----------------------"
+ echo "     SYSTEM INFORMATION"
+ echo "   -----------------------"
  echo  ""
  echo -e "\\e[94m     \\e[39m$MODEL"
  echo -e "\\e[94m     \\e[39m$DISTRO"
@@ -55,12 +55,12 @@ clear # clear the screen first before processing output.
  echo -e "\\e[94m     \\e[39m$CPU [$TEMP.0°C]"
  echo -e "\\e[94m     \\e[39m$GPU" 
  echo -e "\\e[94m     \\e[39m"$MEMORY"G Free" 
- echo -e "\\e[94m   --------------------"
+ echo -e "\\e[94m   ---------------------"
  echo -e "\\e[94m     \\e[39mPlasma $DE"
- echo -e "\\e[94m     \\e[39m$ICONS "
- echo -e "\\e[94m     \\e[39m$COLORS "Scheme""
- echo -e "\\e[94m     \\e[39m$BIRTH"
- echo -e "\\e[94m   --------------------"
+ echo -e "\\e[94m   𝐅  \\e[39m$FONT" 
+ echo -e "\\e[94m   ⬢  \\e[39m$ICONS "
+ echo -e "\\e[94m     \\e[39m$COLORS "Light""
+ echo -e "\\e[94m   ---------------------"
  echo -e "\\e[94m     \\e[39m$Packages"
  echo -e "\\e[94m   $m_ICON  \\e[39m$Playing"
  echo  ""
