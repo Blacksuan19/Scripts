4a4698425a28b2007ef713700669490143f1c883
blacksuan19 <abubakaryagob@gmail.com>
2018-07-30 22:05:56 +0800

    fresh: some new stuff
    fack i wrote my email wrong
    Signed-off-by: Blacksuan19 <abubakryagob@gmail.com>

--- fresh.sh
+++ fresh.sh
@@ -1,5 +1,5 @@
-#! /usr/bin/bash
-# new arch (probably manjaro)machine setup script.
+#!/bin/bash
+# new arch (probably manjaro) machine setup script.
 echo "this script will do some stuff you do when you install a new distro (reinstall software, configure mirrors etc..."
 sleep 2
 echo "enabling custom repo's..."
@@ -18,21 +18,40 @@ Server = https://repo.herecura.be/herecura-testing/x86_64
 EOT
 echo "switching to unstable branch (this might take a while)..."
 sleep 2
-# switch to the unstable branch.
+# switch to the unstable branch(i like living on the edge).
 sudo pacman-mirrors -aB unstable
 echo "updating new databases..."
 sleep 2
 # update the databases.
-sudo pacman -Syyu 
-echo "the old packages list should be named as old.txt"
-sleep 2
-# get list of installed packages for comparison.
-pacman -Q > new.txt
-# get the difference between the two files and also delete the trailing package version numbers and the small sign and output it to a file.
-diff new.txt old.txt | grep '^[<]' | sed 's/1234567890.//g' | sed 's/<//g' > diff.txt 
-# install only the packages available on the pacman repo's and not already installed.
-sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort diff.txt))  
-# install aur packages after filtering them. 
-sudo yaourt -S --needed $(comm -12 <(yaourt -Slq | sort) <(sort diff.txt)) 
+sudo pacman -Syyu pacaur 
+echo "installing some common apps..."
+sudo pacaur -S zsh google-chrome-dev termite spotify telegram-desktop wps-office sublime-text-dev
+echo "setting default shell..."
+chsh -s /bin/zsh
+echo "Setting up based on dotfiles..."
+#powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd -
+rm -rf fonts
+
+#my dotfiles
+git clone https://github.com/Blacksuan19/Dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+ln  .tmux.conf ~/.tmux.conf
+ln  .tmux.conf.local ~/.tmux.conf.local
+cp config ~/.config/termite/
+cp .dircolors ~/.dircolors
+cp .zsh ~/.zsh
+ln .zsh/config.zsh ~/.zsh/config/zsh
+cp .zprezto ~/.zprezto
+cp .z* ~/ # all dem zsh files
+cp -
+git clone https://github.com/Blacksuan19/Scripts.git
+cd Scripts
+sudo cp Spotify.sh /bin/sp
+cp Lyrics.sh ~/.lyrics
+cd -
 echo "script finished"
 echo "new machine setup completed."
