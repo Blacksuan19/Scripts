546ded08d55b2098b953e94e10701667b65686d5
Blacksuan19 <Abubakaryagob@gmail.com>
2018-09-07 03:11:53 +0800

    Massive Update
    * fixed unstable repo enabling issue
    * fixed linking of dotfiles
    * install themes
    * install Dex KDE Icons
    * some cleanup
    
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- fresh.sh
+++ fresh.sh
@@ -5,13 +5,17 @@ sleep 2
 echo "enabling custom repo's..."
 sleep 2
 # copy the lines to the end of pacman config to enable these repo's 
-cat <<EOT >> /etc/pacman.conf 
+
+#some variables
+
+HOME=/home/dark-emperor
+sudo cat <<EOT >> /etc/pacman.conf 
 
 [blackeagle-pre-community]
-Server = http://repo.herecura.be/$repo/$arch
+Server = http://repo.herecura.be/blackeagle-pre-community/x86_64
 
 [herecura]
-Server = http://repo.herecura.be/$repo/$arch
+Server = http://repo.herecura.be/herecura/x86_64
 
 [herecura-testing]
 Server = https://repo.herecura.be/herecura-testing/x86_64
@@ -25,10 +29,11 @@ sleep 2
 # update the databases.
 sudo pacman -Syyu pacaur 
 echo "installing some common apps..."
-sudo pacaur -S zsh google-chrome-dev termite spotify telegram-desktop wps-office sublime-text-dev
+pacaur -S zsh google-chrome-dev termite spotify telegram-desktop wps-office sublime-text-dev tmux redshift tmux
 echo "setting default shell..."
 chsh -s /bin/zsh
 echo "Setting up based on dotfiles..."
+
 #powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -37,21 +42,45 @@ cd -
 rm -rf fonts
 
 #my dotfiles
-git clone https://github.com/Blacksuan19/Dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
-ln  .tmux.conf ~/.tmux.conf
-ln  .tmux.conf.local ~/.tmux.conf.local
-cp config ~/.config/termite/
-cp .dircolors ~/.dircolors
-cp .zsh ~/.zsh
-ln .zsh/config.zsh ~/.zsh/config/zsh
-cp .zprezto ~/.zprezto
-cp .z* ~/ # all dem zsh files
+git clone https://github.com/Blacksuan19/Dotfiles.git $HOME.dotfiles
+cd $HOME/.dotfiles
+ln .tmux.conf $HOME
+ln .tmux.conf.local $HOME
+cp .zshrc $HOME/ # all dem zsh files
+rm $HOME/.zprezto/runcoms/zpreztorc
+cp .zpreztorc $HOME/.zprezto/runcoms/zpreztorc
+rm $HOME/.zprezto/modules/prompt/external/agnoster/agnoster.zsh-theme
+cp agnoster.zsh-theme $HOME/.zprezto/modules/prompt/external/agnoster
+cp config $HOME/.config/termite/
+cp .dircolors $HOME/.dircolors
+mkdir $HOME/.zsh
+ln .config.zsh $HOME/.zsh/config/zsh
+cp command-time.plugin.zsh $HOME/.zsh/command-time.plugin.zsh
+git clone https://github.com/zdharma/fast-syntax-highlighting.git $HOME/.zsh/fast-syntax-highlighting
 cp -
+
 git clone https://github.com/Blacksuan19/Scripts.git
 cd Scripts
 sudo cp Spotify.sh /bin/sp
-cp Lyrics.sh ~/.lyrics
+chmod +x /bin/sp
+cp Lyrics.sh $HOME/.lyrics
 cd -
+echo "Done setting up dotfiles"
+
+echo " configuring themes..."
+#desktop themes
+git clone https://github.com/Blacksuan19/Plasma-Themes.git
+cd Plasma-Themes
+mkdir $HOME/.local/share/plasma/desktoptheme
+cp -r * $HOME/.local/share/plasma/desktoptheme/
+cd - 
+rm -rf Plasma-Themes
+
+#icon pack
+git clone https://github.com/ishovkun/Dex.git
+cd Dex
+sudo cp -r * /usr/share/icons
+cd -
+rm -rf Dex
 echo "script finished"
 echo "new machine setup completed."
