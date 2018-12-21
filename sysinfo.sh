50e7990b9ba3a64731438168c13d25fd457979bf
Abubakar Yagoub <Abubakaryagob@gmail.com>
2018-12-22 01:07:23 +0800

    sysinfo: awk Squash
    * replace all cats with awk
    * split model from vendor variable
    * remove uptime(tmux bar got that covered)
    * remove termite font option(fully on konsole now)
    * get konsole font from profile config file
    * RAM: show total and free
    * add root total and free
    * add more comments where appropriate
    
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,34 +1,25 @@
 #! /bin/sh
 # dark shell grave. 
-# to rework
-#GPU, DE, replace cat with awk
 
-
-OS=$(hostnamectl | awk '{$1=$3="";sub(/^[ \t]+/, "")}NR==7' | sed 's/System:  //g')
-DISTRO=$(lsb_release -sirc | awk '{print $3 " " $2}'  | sed 's/-rc//g')
-KERNEL=$(hostnamectl | awk -F- '/Kernel/{ OFS="-";NF--; print }'|awk '{print $3}')
-UPTIME=$(awk '{printf("%dd %02dh %02dm",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime)
-MODEL=$(cat /sys/devices/virtual/dmi/id/board_{name,vendor} | awk '!(NR%2){print$1,p}{p=$0}')
-DE=$(plasmashell --version | awk '{print $2}') # i use kde plasma, feel free to change this according to your DE/WM.
-CPU=$(awk < /proc/cpuinfo '/model name/{print $5}' | head -1)
-TEMP=$(cat /sys/class/hwmon/hwmon0/temp1_input | awk '{print $1 / 1000}')
-GPU=$(lspci | awk '/VGA/{print $11,$12,$13}' | tr -d '[]')
-MEMORY=$(cat /proc/meminfo | grep MemAvailable | awk '$2 { print substr($2/1000/1000,1,4)}')
+OS=$(hostnamectl | awk '{$1=$3="";sub(/^[ \t]+/, "")}NR==7' | sed 's/System:  //g') # get operating system
+DISTRO=$(lsb_release -sirc | awk '{print $3 " " $2}'  | sed 's/-rc//g') # get distro (this is for non lts )
+KERNEL=$(hostnamectl | awk -F- '/Kernel/{ OFS="-";NF--; print }'|awk '{print $3}') # get kernel version
+MODEL=$(awk </sys/devices/virtual/dmi/id/board_name '{print $1}')  # get device model
+VENDOR=$(awk </sys/devices/virtual/dmi/id/board_vendor '{print $1}')  # get device vendor
+DE=$(plasmashell --version | awk '{print $2}') # i use kde plasma, feel free to change this according to your DE/WM. // slow
+CPU=$(awk < /proc/cpuinfo '/model name/{print $5}' | head -1) # get cpu model
+TEMP=$(awk </sys/class/hwmon/hwmon0/temp1_input '{print $1 / 1000}') # get cpu temp
+GPU=$(lspci | awk '/VGA/{print $11,$12,$13}' | tr -d '[]') # slow af
+ROOT=$(df -h | awk '/^\/dev\/sda1/ {print $3"B/"$2"B"}') # replace /dev/sda1 wwith your root disk name
+MEMORY_TOTAL=$(awk </proc/meminfo '/MemTotal/{ print substr($2/1000/1000,1,4)}') # total memory
+MEMORY=$(awk </proc/meminfo '/MemAvailable/{ print substr($2/1000/1000,1,4)}') # free memory
 SHELL=$(zsh --version | awk '{sub(".", substr(toupper($i),1,1) , $i); print $1" "$2}') # i use zsh if you use another shell change this accordingly.
-Packages=$(pacman -Q | awk 'END {print NR}') # if you dont use arch then what??
-ICONS=$(cat ~/.kde4/share/config/kdeglobals | grep Theme | sed 's/Theme=//g')
-COLORS=$(cat ~/.kde4/share/config/kdeglobals | grep ColorScheme | sed 's/ColorScheme=//g')
-FONT=$(cat ~/.kde4/share/config/kdeglobals | grep font | sed 's/font=//g' | sed 's/,7.5,-1,5,57,0,0,0,0,0,Medium//g')
-WIDGET=$(cat ~/.kde4/share/config/kdeglobals | grep widgetStyle | sed 's/widgetStyle=//g')
-#get current terminal font (konsole and termite only)
-if pgrep -x "konsole" > /dev/null
-then
-	TERM_FONT=$(cat ~/.kde4/share/config/kdeglobals | grep fixed | sed 's/fixed=//g' | sed 's/,8,-1,5,50,0,0,0,0,0,Book//g') # change the sed according to your kdeglobals file the numbers dont always match
-else if pgrep -x "termite" > /dev/null
-then
-	TERM_FONT=$(cat ~/.config/termite/config| grep font | sed 's/font = //g')
-fi
-fi
+Packages=$(pacman -Q | awk 'END {print NR}') # if you dont use pacman then what??
+ICONS=$(awk <~/.kde4/share/config/kdeglobals '/Theme/{print $1}' | sed 's/Theme=//g; s/-icon-theme//g; s/[-]/ /g') # get plasma icons 
+COLORS=$(awk <~/.kde4/share/config/kdeglobals '/ColorScheme/{print $1}' | sed 's/ColorScheme=//g') # get plasma color scheme
+FONT=$(awk <~/.kde4/share/config/kdeglobals '/font/{print $1}' | sed 's/,7.5,-1,5,57,0,0,0,0,0,Medium//g; s/font=//g') # get plasma fonts
+WIDGET=$(awk <~/.kde4/share/config/kdeglobals '/widgetStyle/{print $1}' | sed 's/widgetStyle=//g') # get plasma widgets
+TERM_FONT=$(awk <~/.local/share/konsole/Shell.profile '/Font=/{print $1}' | sed 's/,9,-1,5,50,0,0,0,0,0,Regular//g; s/Font=//g') #change profile name according to yours 
 # get currently playing song (spotify and juk only).
 if pgrep -x "spotify" > /dev/null
 then
@@ -58,14 +49,14 @@ clear # clear the screen first before processing output.
  echo "   SYSTEM INFORMATION"
  echo "   --------------------"
  echo  ""
- echo -e "\\e[94m   \\e[39m$MODEL"
+ echo -e "\\e[94m   \\e[39m$VENDOR $MODEL"
  echo -e "\\e[94m   \\e[39m$DISTRO"
  echo -e "\\e[94m   \\e[39m$OS$KERNEL"
- echo -e "\\e[94m   \\e[39m$UPTIME"
  echo -e "\\e[94m   \\e[39m$SHELL"
  echo -e "\\e[94m   \\e[39m$CPU [$TEMP.0°C]"
  echo -e "\\e[94m   \\e[39m$GPU" 
- echo -e "\\e[94m   \\e[39m"$MEMORY"G Free" 
+ echo -e "\\e[94m   \\e[39m"$MEMORY"G/"$MEMORY_TOTAL"G Free" 
+  echo -e "\\e[94m   \\e[39m"$ROOT" Free" 
  echo -e "\\e[94m   ---------------------"
  echo -e "\\e[94m   \\e[39mPlasma $DE"
  echo -e "\\e[94m   \\e[39m$WIDGET Style" 
