8b89e99d11a4872bc039a3d1362c53e1edfbf549
blacksuan19 <i@blacksuan19.me>
2020-05-17 00:12:16 +0800

    da: simplify commit message
    just show the message
    
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- dark_ages_ci.sh
+++ dark_ages_ci.sh
@@ -5,7 +5,7 @@ BOT=$BOT_API_KEY
 KERN_IMG=$PWD/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$HOME/Zipper
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-COMMIT=$(git log --pretty=format:'"%h : %s"' -1 | sed -e 's|["'\'']||g')
+COMMIT=$(git log --pretty=format:'%s' -1)
 THREAD=-j$(nproc --all)
 DEVICE=$1
 [ -z "$DEVICE" ] && DEVICE="vince" # if no device specified use vince
