04490f9b7370b1d26735fad03e2c33295e4aabff
blacksuan19 <i@blacksuan19.me>
2020-09-30 05:26:14 +0800

    havoc: cherry-pick 120hz string
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- build_havoc.sh
+++ build_havoc.sh
@@ -25,6 +25,12 @@ clone "sarveshrulz/android_vendor_aeonax_anxcamera" "vendor/aeonax/ANXCamera" "-
 clone "Havoc-OS/android_external_motorola_faceunlock" "external/motorola/faceunlock"
 bash external/motorola/faceunlock/regenerate/regenerate.sh
 
+# custom havoc settings (for 120hz strings)
+cd packages/apps/Settings
+git fetch https://github.com/Blacksuan19/android_packages_apps_Settings
+git cherry-pick 1a4f15ea4f4ceef8a44af99282589e12b8576705
+
 # build rom
+cd -
 . build/envsetup.sh
 brunch phoenix
